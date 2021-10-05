304f57d91ae78ac60906372c8dc0fddd9ee3c428
nizaguir <nmaijo@icloud.com>
2021-10-04 19:43:55 -0500

    margin on footer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// The year goes according to the calendar date year 
 document.querySelector("#theyear").textContent = new Date().getFullYear()
 
+// Date of last modification
 document.querySelector("#lastmoddate").textContent = document.lastModified
