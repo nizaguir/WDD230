cf53a7a209ab07e5a709af13132bf03dd1a77f34
nizaguir <nmaijo@icloud.com>
2021-10-16 14:08:33 -0500

    update folder name and styling the footer

--- lesson5/js/bannerad.js
+++ lesson5/js/bannerad.js
@@ -1,20 +1,13 @@
-// determine what day of the week it is today
-// let thedate = new Date ();
-// console.log(thedate.getDay());
 
-//change to 5 because it equals Friday
-// in order to do in one line use ternary operator
-// check codepen 
-
-if (thedate.getDay() == 5) {
-  document.querySelector('#banner').style.display = "block";
-}
-
-
-// const bannerEl = document.querySelector('#banner');
-// const newDateObj = new Date();
-// const today = newDateObj.getDay();
-
-// if (today == 5){
-//     bannerEl.style.display = "block";
+// if (thedate.getDay() == 5) {
+//   document.querySelector('#banner').style.display = "block";
 // }
+
+
+const bannerad = document.querySelector('#banner');
+const newDateObj = new Date();
+const today = newDateObj.getDay();
+
+if (today == 5){
+    bannerad.style.display = "block";
+}
