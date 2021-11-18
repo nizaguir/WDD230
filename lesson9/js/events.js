e6735b1dc0398a539c09e6d8c37d1a2f6c354f9d
nizaguir <nmaijo@icloud.com>
2021-11-18 11:45:29 -0600

    event styling

--- lesson9/js/events.js
+++ lesson9/js/events.js
@@ -27,7 +27,7 @@ fetch(infoURL)
 
         // Create Event Cards //
         let sectionEvent = document.createElement("section");
-        let h4Event = document.createElement("h4");
+        let h3Event = document.createElement("h3");
         let divContCardEvent = document.createElement("div");
         let events = town.events;
         let allEvents = document.createElement("div");
@@ -39,7 +39,7 @@ fetch(infoURL)
           "events"
         );
 
-        h4Event.textContent = town.name;
+        h3Event.textContent = town.name;
 
         events.forEach((event) => {
           let eventPar = document.createElement("p");
@@ -48,8 +48,10 @@ fetch(infoURL)
         });
 
         allEvents.setAttribute("class", "events-name");
+        h3Event.setAttribute("class","h3-title");
+        // eventPar.setAttribute("class", "p-event");
         divContCardEvent.append(allEvents);
-        sectionEvent.append(h4Event, divContCardEvent);
+        sectionEvent.append(h3Event, divContCardEvent);
         document.querySelector("div.town-events-info").append(sectionEvent);
       }
     });
