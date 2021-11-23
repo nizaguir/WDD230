fbd581fd06230c6674bad9b9c64fbeb2f796e64c
nizaguir <nmaijo@icloud.com>
2021-11-22 22:34:37 -0600

    corrected city id for soda springs and fish haven

--- lesson11/soda_springs/js/weather.js
+++ lesson11/soda_springs/js/weather.js
@@ -1,5 +1,5 @@
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=1c7b520dbc19d236d0b08977b014ccf1";
+  "https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=1c7b520dbc19d236d0b08977b014ccf1";
 
 fetch(apiURL)
   .then((response) => response.json())
